2c550a1c36af335782d9ba5283ec1a0ecefc1656
pouryourlove <lqu134@naver.com>
2024-07-02 22:52:41 +0900

    streams

---
+++ 14.Streams/index.js
@@ -0,0 +1,7 @@
+import { createReadStream } from "fs";
+
+// const stream = createReadStream('./data.txt', {highWaterMark: 90000})//control the size of the buffer
+
+const stream = createReadStream('./data.txt', {encoding: 'utf8'})
+
+stream.on('data', (data) => console.log(data))
